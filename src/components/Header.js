18a1fc3fcf2bbd2dd71820a0724d25f97c65ede8
xeno <kerasidisxenokratis@gmail.com>
2023-07-25 14:26:14 +0300

    Tailwind setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import React from 'react'
 function Header() {
   return (
     <header>
-        <h1>
+        <h1 class="text-4xl"> 
             Simple ToDo App!
         </h1>
+        <br/>
     </header>
   )
 }
