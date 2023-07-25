16f7f75213e3adebb2ac89c0fe1595db5c0a4616
xeno <kerasidisxenokratis@gmail.com>
2023-07-25 17:41:59 +0300

    added independent delete button for each task

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 import Header from './components/Header';
-import { useState } from 'react';
-import data from './data.json'
+import data from './data.json';
 import ToDoList from './components/ToDoList';
 
 function App() {
   const [toDoList, setToDoList] = useState(data);
-  
 
   const handleLineThrough = (id) => {
     let mappedStriking = toDoList.map(task => {
-      return task.id == id ? {...task, complete: !task.complete} :{...task};
-    })
+      return task.id === id ? { ...task, complete: !task.complete } : { ...task };
+    });
     setToDoList(mappedStriking);
-  }
+  };
 
   const handleClear = () => {
     let cleared = toDoList.filter(task => {
       return !task.complete;
-    })
-    setToDoList(cleared)
-  }
+    });
+    setToDoList(cleared);
+  };
+
+  const handleDelete = (id) => {
+    let filteredList = toDoList.filter(task => task.id !== id);
+    setToDoList(filteredList);
+  };
 
   return (
     <div>
-      <Header/>
-      <ToDoList toDoList={toDoList} handleLineThrough={handleLineThrough} handleClear={handleClear}/>
-      
+      <Header />
+      <ToDoList
+        toDoList={toDoList}
+        handleLineThrough={handleLineThrough}
+        handleClear={handleClear}
+        handleDelete={handleDelete}
+      />
     </div>
-    
-  )
+  );
 }
 
 export default App;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,39 +1,35 @@
 import React from 'react';
-import data from '../data.json'
 
-// const ToDo = ({ todo }) => {
-//   return (
-//     <div>
-//       {todo.task}
-//     </div>
-//   );
-// };
-
-
-//map through tasks
-const ToDoList = ({ toDoList, handleLineThrough, handleClear }) => {
+const ToDoList = ({ toDoList, handleLineThrough, handleClear, handleDelete }) => {
   return (
     <div>
       {toDoList.map(todo => {
         return (
-          <div
-            key={todo.id}
-            className={`font-medium ${todo.complete ? "line-through" : ""} cursor-pointer`}
-            onClick={() => handleLineThrough(todo.id)} 
-          >
-            {todo.task}
+          <div key={todo.id} className={`flex items-center mb-2`}>
+            <div
+              className={`font-medium cursor-pointer ${todo.complete ? "line-through" : ""}`}
+              onClick={() => handleLineThrough(todo.id)}
+            >
+              {todo.task}
+            </div>
+            <button
+              className="ml-2 px-2 py-1 bg-red-500 hover:bg-red-600 text-white font-medium rounded-md shadow-md focus:outline-none focus:ring focus:ring-red-300"
+              onClick={() => handleDelete(todo.id)}
+            >
+              Delete
+            </button>
           </div>
         );
-      })} <br/>
+      })}
+      <br />
       <button
-      onClick={handleClear}
-      className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md shadow-md focus:outline-none focus:ring focus:ring-blue-300"
-    >
-        Clear!
+        onClick={handleClear}
+        className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-md shadow-md focus:outline-none focus:ring focus:ring-blue-300"
+      >
+        Clear Completed
       </button>
     </div>
   );
 };
 
 export default ToDoList;
-
